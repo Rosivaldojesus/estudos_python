f2522cde0883267731627fa53a08782c12ad5f78
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-12-13 23:23:51 -0300

    add decorators

--- SQL_Server/conectar_db.py
+++ SQL_Server/conectar_db.py
@@ -1,18 +1,17 @@
 # Instalar: pyodbc
 
+
 import pyodbc
 
 dados_conexao = (
-    "Driver={SQL Server};"
-    "Server=DESKTOP-HVTNLUB\SQLEXPRESS;"
-    "Database=PythonSQL;"
+    'Driver={SQL Server};'
+    'Server=DESKTOP-HVTNLUB\SQLEXPRESS;'
+    'Database=PythonSQL;'
 )
 
 conexao = pyodbc.connect(dados_conexao)
 
 print('Conexão bem sucedida!!!')
-
-
 
 cursor = conexao.cursor()
 cursor.execute('SELECT * FROM Vendas')
---
+++ decorator/decoradores_02.py
@@ -0,0 +1,23 @@
+"""
+Decorators com diferentes assinaturas
+- Usando decorator Pattern
+"""
+
+
+def gritar(funcao):
+    def aumentar(*args, **kwargs):
+        return funcao(*args, **kwargs).upper()
+    return aumentar
+
+
+@gritar
+def saudacao(nome):
+    return f'Olá, eu sou o/a {nome}'
+
+
+@gritar
+def ordenar(principal, acompanhamento):
+    return f'Olá, eu gostaria de {principal}, acompanhado de {acompanhamento}, por favor.'
+
+
+print(ordenar('Picanha', 'Batata Frita'))
