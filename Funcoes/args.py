2c4e6ef735d01e80feb88a1e34c7e1f529c30613
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-12-14 09:49:35 -0300

    add kwargs

---
+++ Funcoes/args.py
@@ -0,0 +1,38 @@
+"""
+ - *args: (Argumentos sem palavra-chave)
+
+ A sintaxe especial *args nas definições de função em python é usada para passar um número variável de argumentos para
+ uma função. É usada para passar uma lista de argumentos de comprimento variável e sem palvras-chave.
+
+    - A sintaxe é usar um o símbolo * para obter um números variável de argumentos: poe convenção, é frequentemente
+    usado com a palavras args.
+    - O que *args permite que você faça é aceitar mais argumentos do que o número de argumentos formais que você
+    definiu anteriormente. Com o *args, qualquer número de argumentos extras pode ser adicionado aos seus parâmetros
+    formais atuais (incluindo nenhum argumento extra).
+    - Por exemplo: queremos fazer uma função de multiplicação que recebe qualuer número de argumentos e seja capaz de
+    multiplicá-los todos juntos. Isso pode ser feito usando o *args
+    - Usando *, a variável que associamos com * torna-se um significado iterável, você pode fazer coisas como interar
+    sobre ela, executar algumas funções de ordem superior, como mapear e filtar, etc.
+"""
+
+
+#  Exemplo 01
+
+
+def minha_funcao_1(*args):
+    for arg in args:
+        print(arg)
+
+
+minha_funcao_1('Olá', 'Bem-vindo', 'para', 'terra')
+print("-------------------------------------------------------------------------------")
+#  Exemplo 02
+
+
+def minha_funcao_2(argumento1, *args):
+    print("Primeiro argumento: ", argumento1)
+    for arg in args:
+        print("Próximo argumento por meio de *args: ", arg)
+
+
+minha_funcao_2('Olá', 'Bem-vindo', 'para', 'terra')
---
+++ Funcoes/kwargs.py
@@ -0,0 +1,22 @@
+"""
+**kwargs (Argumentos de palavra-chave)
+
+ - A sintaxe especial **kwargs nas definições de função em python é usada para passar uma lista de argumentos de
+ comprimento variável com palavras-chave. Usamos o nome kwargs com a estrela dupla. A razão é porque a estrela dulpla
+ permite passar por argumentos de palvras-vhave ( e qulauer número deles).
+
+  - Um argumento de palavra-chave é onde você fornece um nome para a variável ao passá-la para a função.
+  - Pode-se pensar nos kwargs como um dicionário que mapeia cada palavra-chave para o valor que passamos junto com ela.
+  É por isso que, quando iteramos sobre os kwargs, não parece haver nenhuma ordem que foram impressoa.
+
+"""
+#  Exemplo de uso de **kwargs
+
+
+def minha_funcao(**kwargs):
+    for key, value in kwargs.items():
+        print(f'{key} == {value}')
+
+
+
+minha_funcao(Primeiro='Geeks', Meio='for', ultimo='Geeks')
