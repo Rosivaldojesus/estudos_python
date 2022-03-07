87be35d1c83813802fff019893b633eae73bc549
Rosivaldojesus <js.rosivaldo@gmail.com>
2022-03-06 21:52:03 -0300

    Expressoes regulares

---
+++ barra_progresso/barra_progresso_02.py
@@ -0,0 +1,13 @@
+from tqdm import tqdm
+import time
+
+
+novos_produtos = []
+
+produtos = ['Uva', 'Laranja', 'Manga', 'Goiba', 'Melancia', 'Morango']
+
+for produto in tqdm(produtos):
+    novos_produtos.append(produto)
+    time.sleep(1)
+
+print(novos_produtos)
