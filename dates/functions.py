59eaaf621d508fd61308d2703cb06c98ae8f2147
Rosivaldojesus <js.rosivaldo@gmail.com>
2022-04-09 23:51:42 -0300

    Date

---
+++ dates/functions.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+def today():
+    today = datetime.now()
+    return today
+
---
+++ dates/main_functions_date.py
@@ -0,0 +1,9 @@
+from functions import today
+
+print("###########################")
+print("Qual a data de vencimento? ")
+print("###########################")
+
+due_date = input("Qual a data de vencimento?: ")
+
+print(today())
